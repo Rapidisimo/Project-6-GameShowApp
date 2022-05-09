0966df485cd47ae0d0eb231ad3a9103d6a6348ac
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2022-05-08 20:28:18 -0400

    added comments to my code and fixed a 'bug'

--- js/app.js
+++ js/app.js
@@ -10,22 +10,22 @@ let letterMatch = null;
 const hearts = document.querySelectorAll('.tries');
 const title = document.querySelector('.title');
 
-
+// Listens for a click on the overlay button and depending on it's text it either starts the game or resets it.
 startGame.addEventListener ('click', (event) => {
     if ( event.target.innerHTML === 'Start Game') {
         overlay.style.display = 'none';
     } else if ( event.target.innerHTML === 'Replay?') {
         location.reload();
     }
-    
 } )
 
+// Gets a random phrase from phrases constant and creates an array of letters from it.
 function getRandomPhraseAsArray (arr) {
     const randomP = Math.floor(Math.random() * arr.length );
     const letras = Array.from(arr[randomP]);
     return letras
 }
-
+// Creates list items bases on the resulting letter array. If it's a space it assigns space as a class if not then it gets assigned a letter class.
 function addPhraseToDisplay(arr) {
     const list = document.querySelector('#phrase ul');
 
@@ -41,13 +41,17 @@ function addPhraseToDisplay(arr) {
         }
         return arr;
 }
+//Runs the function to create/display the phrase to guess
 addPhraseToDisplay(phraseArray);
 
-
+//Listens to clicks for onscreen keyboard buttons, assigns "chosen" class and disables button it for futher use.
+//Sends the innerHTML of the button the the checkLetter function for comparison. If they are the same it's class is update and the function to check for a win is called.
+//If they don't match, the missed counter is increased, the class is updated, the badGuess function is called to remove a heart and checkWin function is called to see if they lost.
 qwerty.addEventListener('click', (event) => {
 
-   if (event.target.tagName === 'BUTTON') {
+   if (event.target.tagName === 'BUTTON') {e
         event.target.className = 'chosen';
+        event.target.disabled = true;
         let buttonLetter = event.target.innerHTML;
         checkLetter(buttonLetter);
         if (letterMatch === buttonLetter) {
@@ -62,6 +66,8 @@ qwerty.addEventListener('click', (event) => {
    }
 })
 
+// Called by the keyboard event listener.
+// Checks to see if the letter of the button matches the letter in the li not being shown. If it does then a class is added to show it on the board.
 function checkLetter (arr) {
     const letters = document.querySelectorAll('.letter'); 
 
@@ -72,7 +78,8 @@ function checkLetter (arr) {
         } 
     } return letterMatch
 }
-
+// Called by the keyboard event listener.
+// If the letter clicked wasn't a match then this function increases the missed count and then removes a heart from the board.
 function badGuess (arr) {
     let score = missed - 1;
     for (let i = score; i < arr.length; i++ ) {
@@ -82,7 +89,10 @@ function badGuess (arr) {
         }
     }
 }
-
+// Called by the keyboard event listener.
+// Checks to see if you have won or lost the game by comparing the count of the initial set of letters to
+// the count of letters you have guessed correctly. It also changes the overlay screen to a win or lose version and the button text/behaviour.
+// I also added a delay when winning or losing so you can see the completed phrase(if you won).
 function checkWin () {
     let lettersShown = document.querySelectorAll('.show');
     let letter = document.querySelectorAll('.letter');
